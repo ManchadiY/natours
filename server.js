82fc36d5fdd97b83123a94accf8c3d88e2ab843d
Yuvraj Manchadi <yuvrajmanchadi321@gmail.com>
2024-10-28 14:53:36 +0530

    env update

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose
 //console.log(process.env); // node env
 
 //server port & listen
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`listening on the port ${port} ....`);
 });
